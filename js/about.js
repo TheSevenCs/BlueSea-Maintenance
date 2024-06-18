98455ce394c1a07a5e0cab8f6135a5fdca9029ea
Andrew Lee <leeandrew135@gmail.com>
2024-06-18 15:58:44 -0400

    updates to header section

--- js/about.js
+++ js/about.js
@@ -1,24 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const animatedElement = document.querySelector(".animated-element");
-
-//   // Delay to simulate loading or a specific trigger
-//   setTimeout(() => {
-//     animatedElement.classList.add("visible");
-//   }, 500); // Adjust the delay as needed
-// });
-
-// function animation1() {
-//   var box = document.getElementById("animation1");
-
-//   if (box.style.transform === "translateX(100px)") {
-//     box.style.transform = "translateX(0)";
-//     box.style.opacity = "1";
-//   } else {
-//     box.style.transform = "translateX(100px)";
-//     box.style.opacity = "0.5";
-//   }
-// }
-
 document.addEventListener("DOMContentLoaded", function () {
   var elements = document.querySelectorAll(".animated-center");
 
